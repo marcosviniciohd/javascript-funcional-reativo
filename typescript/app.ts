e8ddd7ccdba9b58d2754a4dc3f7276cb568e63eb
Marcos <marcosviniciohd@hotmail.com>
2022-05-05 17:22:40 -0300

    Iniciando o curso de Angular da cod3r

---
+++ typescript/app.ts
@@ -0,0 +1,14 @@
+let menssage: string = "Hello World";
+console.log(menssage);
+
+let episode: number = 4;
+console.log("O episodio é " + episode);
+episode = episode + 1;
+console.log("Próximo episodio " + episode);
+episode = '10';
+console.log(episode);
+
+let qualquerTipo: string
+qualquerTipo = "Olá Mundo!"
+qualquerTipo = 100
+console.log(qualquerTipo);
---
+++ typescript/dist/app.js
@@ -0,0 +1,12 @@
+var menssage = "Hello World";
+console.log(menssage);
+var episode = 4;
+console.log("O episodio é " + episode);
+episode = episode + 1;
+console.log("Próximo episodio " + episode);
+episode = '10';
+console.log(episode);
+var qualquerTipo;
+qualquerTipo = "Olá Mundo!";
+qualquerTipo = 100;
+console.log(qualquerTipo);
